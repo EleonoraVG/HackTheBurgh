7d9db4c4eb14ac8a6603558eb44ff44d647492d5
Andrea D'Olimpio <andrea.dolimpio@gmail.com>
2019-03-16 18:40:12 +0000

    allow querying user with POST

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bson.objectid import ObjectId
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
 from flask import url_for
 from pymongo import MongoClient
 
@@ -17,13 +19,22 @@ db = client.hacktheburgh
 def homepage():
     return render_template("index.html")
 
-#TODO(andrea): change so that user_id does not appear in URL, ie use RESTful api
+# TODO(andrea): Debug only. Remove in prod
 @app.route("/u/<user_id>")
 def userpage(user_id):
-    print(user_id)
     user = db.users.find_one({"_id": ObjectId(user_id)})
-    print(user)
     return render_template("user.html", user=user)
+
+@app.route('/', methods=['POST'])
+def user_request():
+    data = request.form
+    if data and data.get('uid'):
+        uid = data.get('uid')
+        print(uid)
+        user = db.users.find_one({"_id": ObjectId(uid)})
+        if user:
+            return render_template("user.html", user=user)
+    return render_template("index.html", error=True)
 
 if __name__ == "main":
     app.run()

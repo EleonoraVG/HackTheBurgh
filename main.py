ccc962566b2855404364974d9dd2a68f06a2c642
Andrea D'Olimpio <andrea.dolimpio@gmail.com>
2019-03-16 16:17:02 +0000

    move to pymongo

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
-from flask_pymongo import PyMongo
 from flask import url_for
+from pymongo import MongoClient
 
 # Flask configurations
 app = Flask(__name__, template_folder='templates')
-app.config["MONGO_URI"] = "mongodb://hacktheburgh-g46bf.gcp.mongodb.net/test"
-mongo = PyMongo(app)
+
+# Mongo config
+client = MongoClient("mongodb+srv://hacktheburgh-g46bf.gcp.mongodb.net")
+db = client.hacktheburgh
 
 # Load landing page
 @app.route('/')
@@ -15,7 +17,8 @@ def homepage():
 #TODO(andrea): change so that user_id does not appear in URL, ie use RESTful api
 @app.route("/u/<user_id>")
 def userpage(user_id):
-    user = mongo.db.users.find_one_or_404({"_id": user_id})
+    print(user_id)
+    user = db.users.find({"_id": user_id})
     return render_template("user.html", user=user)
 
 if __name__ == "main":

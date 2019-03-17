41e0e674719536cbdceaa1e3ff06d5ab20a2436f
eleonora.vangenechten <eleonora.vangenechten@gmail.com>
2019-03-17 11:44:57 +0100

    post redirect works.

--- static/nfc-read.js
+++ static/nfc-read.js
@@ -1,54 +1,49 @@
 // Read from an NFC tag and print to console.
 function readNfc() {
-  if ('nfc' in navigator) {
-    navigator.nfc.watch(function(message) {
-        consoleLog("NFC message received from URL " + message.url);
-        if (message.records[0].recordType == 'empty') {
-          console.log("Tag is empty.");
-        } else {
-          const http_req = new XMLHttpRequest();
-          const url = "/new";
-          var data = {
-            uid: message.records[0].data
-          }
-          http_req.open("POST", url, true);
-          http_req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-          http_req.onreadystatechange = function() {
-            if (http_req.readyState == 4 && http_req.status == 200) {
-              // do something with response
-              console.log(http_req.responseText);
-            }
-          };
-          http_req.send(data);
-        }
-      }, {
-        mode: 'any'
-      })
-      .then(() => consoleLog("Added a watch."))
-      .catch(err => consoleLog("Adding watch failed: " + err.name));
-  } else {
-    consoleLog('NFC API not supported.');
-  }
+	if ('nfc' in navigator) {
+		navigator.nfc.watch(function(message) {
+				consoleLog("NFC message received from URL " + message.url);
+				if (message.records[0].recordType == 'empty') {
+					console.log("Tag is empty.");
+				} else {
+					var form = document.createElement("form");
+					form.setAttribute("method", "post");
+					form.setAttribute("url", "/");
+
+					var hiddenField = document.createElement("input");
+					hiddenField.setAttribute("type", "hidden");
+					hiddenField.setAttribute("name", "uid");
+					hiddenField.setAttribute("value", "5c8d398b39c16384ceb37c78");
+
+					form.appendChild(hiddenField);
+					document.body.appendChild(form);
+					form.submit();
+				}
+			}, {
+				mode: 'any'
+			})
+			.then(() => consoleLog("Added a watch."))
+			.catch(err => consoleLog("Adding watch failed: " + err.name));
+	} else {
+		consoleLog('NFC API not supported.');
+	}
 }
 
 function consoleLog(data) {
-  console.log(data);
+	console.log(data);
 }
 
 function testPost() {
-  const http_req = new XMLHttpRequest();
-  const url = "/";
-  var data = {
-    uid: "testPost123" 
-  }
-  http_req.open("POST", url, true);
-  http_req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  http_req.onreadystatechange = function() {
-    if (http_req.readyState == 4 && http_req.status == 200) {
-      // do something with response
-      console.log(http_req.responseText);
-      //      window.location.href("user.html", user = user)
-    }
-  };
-  http_req.send(data);
+	var form = document.createElement("form");
+	form.setAttribute("method", "post");
+	form.setAttribute("url", "/");
+
+	var hiddenField = document.createElement("input");
+	hiddenField.setAttribute("type", "hidden");
+	hiddenField.setAttribute("name", "uid");
+	hiddenField.setAttribute("value", "5c8d398b39c16384ceb37c78");
+
+	form.appendChild(hiddenField);
+	document.body.appendChild(form);
+	form.submit();
 }
